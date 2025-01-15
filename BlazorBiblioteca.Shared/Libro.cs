fcb25ea9ff12fec6fafc7e361a08ae55a23a323c
Onofre Davila Morillo <66891673+OnofreDavila@users.noreply.github.com>
2025-01-14 21:42:16 -0300

    Add files via upload

---
+++ BlazorBiblioteca.Shared/Libro.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorBiblioteca.Shared
+{ 
+    public class Libro
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo vacio, ingresa un nombre")]
+        public string? NombreLibro { get; set; } 
+
+        [Required(ErrorMessage = "Campo vacio, ingresa un el nombre del Autor.")]
+        public string? Autor { get; set; }
+    
+        [Required(ErrorMessage = "Campo vacio, ingresa la cantidad de paginas del libro.")]
+        public int NumPaginas { get; set; }
+        public DateOnly FechaPublicacion { get; set; }
+
+
+        public Libro(string nombreLibro, string autor, int numPaginas, DateOnly fechaPublicacion)
+        {
+            NombreLibro = nombreLibro;
+            Autor = autor;
+            NumPaginas = numPaginas;
+            FechaPublicacion = fechaPublicacion;
+        }
+        public Libro() { }
+    }
+}
